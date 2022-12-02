38c4c5784a8eee84bb3409eb8828269a2b4fbea2
nhkim <nhkim1529@gmail.com>
2022-12-02 17:45:04 +0900

    fear: MainActivity Navigation,Fragment 구현

---
+++ app/src/main/java/com/example/communityapp/fragments/HomeFragment.kt
@@ -0,0 +1,51 @@
+package com.example.communityapp.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.communityapp.R
+import com.example.communityapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding : FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        binding.tipTap.setOnClickListener{
+            Log.d("HomeFragment", "tipTap")
+            it.findNavController().navigate(R.id.action_homeFragment_to_tipFragment)
+        }
+
+        binding.talkTap.setOnClickListener{
+            it.findNavController().navigate(R.id.action_homeFragment_to_talkFragment)
+        }
+
+        binding.bookmarkTap.setOnClickListener{
+            it.findNavController().navigate(R.id.action_homeFragment_to_bookmarkFragment)
+        }
+
+        binding.storeTap.setOnClickListener{
+            it.findNavController().navigate(R.id.action_homeFragment_to_storeFragment)
+        }
+
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/example/communityapp/fragments/StoreFragment.kt
@@ -0,0 +1,46 @@
+package com.example.communityapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.communityapp.R
+import com.example.communityapp.databinding.FragmentStoreBinding
+
+class StoreFragment : Fragment() {
+
+    private lateinit var binding : FragmentStoreBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_store, container, false)
+
+        binding.homeTap.setOnClickListener{
+            it.findNavController().navigate(R.id.action_storeFragment_to_homeFragment)
+        }
+
+        binding.tipTap.setOnClickListener{
+            it.findNavController().navigate(R.id.action_storeFragment_to_tipFragment)
+        }
+
+        binding.bookmarkTap.setOnClickListener{
+            it.findNavController().navigate(R.id.action_storeFragment_to_bookmarkFragment)
+        }
+
+        binding.talkTap.setOnClickListener{
+            it.findNavController().navigate(R.id.action_storeFragment_to_talkFragment)
+        }
+        return binding.root
+    }
+
+
+}
